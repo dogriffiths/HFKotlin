13c0da568f0c3b0baa9bf576c67dbb5c8d20bced
David Griffiths <dogriffiths@gmail.com>
2018-11-03 16:05:23 +0000

    test: Add test config for IE

--- Makefile
+++ Makefile
@@ -17,6 +17,10 @@ start: build
 test: build
 	cd tests && ./gradlew test
 
+test_ie:
+	cd tests && ./gradlew --no-daemon --stacktrace -Dselenide.baseUrl='http://192.168.1.2:3000/' -DdriverName=ie -PbuildDir=build_ie test
+#	cd tests && ./gradlew --no-daemon --stacktrace -Dselenide.baseUrl='https://dogriffiths.github.io/HFKotlin' -DdriverName=ie -PbuildDir=build_ie test
+
 storybook: build
 	. "${NVM_DIR}/nvm.sh" && nvm use $(NVM) && yarn storybook
 
--- tests/src/test/java/com/example/steps/Hooks.java
+++ tests/src/test/java/com/example/steps/Hooks.java
@@ -4,13 +4,17 @@ import com.codeborne.selenide.Configuration;
 
 import com.codeborne.selenide.WebDriverRunner;
 import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 
 import java.io.IOException;
+import java.net.URL;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import uk.co.blackpepper.relish.selenide.SelenideWidget;
 
 import static com.codeborne.selenide.Selenide.clearBrowserCookies;
@@ -21,15 +25,44 @@ public class Hooks {
     @Before
     public void before() throws IOException {
         String driver = getDriverName();
-        SelenideWidget.setEdgeQuirksMode(false);
-        Configuration.browser = driver;
-        Configuration.chromeSwitches = "disable-infobars";
-        String property = System.getProperty("selenide.baseUrl");
-        if (property == null) {
-            Configuration.baseUrl = "http://localhost:3000";
+
+        if (isRemote(driver)) {
+            String hub = "http://192.168.1.119:4444/wd/hub";
+            String hubProperty = System.getProperty("hub");
+            if (hubProperty != null) {
+                hub = hubProperty;
+            }
+            WebDriver webDriver = ("ie".equals(driver))
+                    ? new RemoteWebDriver(new URL(hub), DesiredCapabilities.internetExplorer())
+                    : new RemoteWebDriver(new URL(hub), DesiredCapabilities.edge());
+            SelenideWidget.setEdgeQuirksMode("edge".equals(driver));
+            WebDriverRunner.setWebDriver(webDriver);
+            String property = System.getProperty("selenide.baseUrl");
+            if (property == null) {
+                Configuration.baseUrl = "http://localhost:3000";
+            }
+        } else {
+            SelenideWidget.setEdgeQuirksMode(false);
+            Configuration.browser = driver;
+            Configuration.chromeSwitches = "disable-infobars";
+            String property = System.getProperty("selenide.baseUrl");
+            if (property == null) {
+                Configuration.baseUrl = "http://localhost:3000";
+            }
         }
         Configuration.browserSize = "1024x768";
         WebDriverRunner.getWebDriver().manage().window().setPosition(new Point(60, 80));
+    }
+
+    public boolean isRemote(String driver) {
+        return ("edge".equals(driver)) || ("ie".equals(driver));
+    }
+
+    @After
+    public void closeDown() {
+        if (isRemote(getDriverName())) {
+            WebDriverRunner.getWebDriver().close();
+        }
     }
 
     @After
@@ -37,16 +70,12 @@ public class Hooks {
         clearBrowserCookies();
         try {
             clearBrowserLocalStorage();
-        } catch(WebDriverException wde) {
+        } catch (WebDriverException wde) {
             System.err.println("Cannot clear local storage. Non browser test?");
         }
     }
 
     private static String getDriverName() {
-        String driver = System.getProperty("driverName");
-        if (driver == null) {
-            driver = "chrome";
-        }
-        return driver;
+        return System.getProperty("driverName", "chrome");
     }
 }
