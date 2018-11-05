cc64e85f90a63ff96f57ddcbdeae05c2d79a017c
David Griffiths <dogriffiths@gmail.com>
2018-11-05 18:45:16 +0000

    refactor: Pre-load Twitter widget in page

--- src/containers/TwitterTimeline/TwitterTimeline.js
+++ src/containers/TwitterTimeline/TwitterTimeline.js
@@ -1,15 +1,6 @@
 import React, {Component} from 'react';
 
 class TwitterTimeline extends Component {
-    componentDidMount () {
-        const script = document.createElement("script");
-
-        script.src = "https://platform.twitter.com/widgets.js";
-        script.async = true;
-
-        document.body.appendChild(script);
-    }
-
     render() {
         return <div>
             <a className="twitter-timeline" href="https://twitter.com/HeadFirstKotlin?ref_src=twsrc%5Etfw">Tweets by
