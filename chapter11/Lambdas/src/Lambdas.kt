2bc44d11c9ba5b0f6d5d0515909b6cb7b01822ba
David Griffiths <dogriffiths@gmail.com>
2018-11-29 19:47:58 +0000

    feat: Add code for chapter 11

---
+++ chapter11/Lambdas/src/Lambdas.kt
@@ -0,0 +1,45 @@
+typealias DoubleConversion = (Double) -> Double
+
+fun convert(
+    x: Double,
+    converter: DoubleConversion
+): Double {
+    val result = converter(x)
+    println("$x is converted to $result")
+    return result
+}
+
+fun getConversionLambda(str: String): DoubleConversion {
+    if (str == "CentigradeToFarenheit") {
+        return { it * 1.8 + 32 }
+    } else if (str == "KgsToPounds") {
+        return { it * 2.204623 }
+    } else if (str == "PoundsToUSTons") {
+        return { it / 2000.0 }
+    } else {
+        return { it }
+    }
+}
+
+fun combine(
+    lambda1: DoubleConversion,
+    lambda2: DoubleConversion
+): DoubleConversion {
+    return { x: Double -> lambda2(lambda1(x)) }
+}
+
+fun main(args: Array<String>) {
+    //Convert 2.5kg to Pounds
+    println("Convert 2.5kg to Pounds: ${getConversionLambda("KgsToPounds")(2.5)}")
+
+    //Define two conversion lambdas
+    val kgsToPoundsLambda = getConversionLambda("KgsToPounds")
+    val poundsToUSTonsLambda = getConversionLambda("PoundsToUSTons")
+
+    //Combine the two lambdas to create a new one
+    val kgsToUSTonsLambda = combine(kgsToPoundsLambda, poundsToUSTonsLambda)
+
+    //Use the new lambda to convert 17.4 to US tons
+    val value = 17.4
+    println("$value kgs is ${convert(value, kgsToUSTonsLambda)} US tons")
+}
