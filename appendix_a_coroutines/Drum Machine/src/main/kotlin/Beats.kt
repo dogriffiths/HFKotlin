cf65ac226f5857b684e4e886fb2f323fe25a56b4
David Griffiths <dogriffiths@gmail.com>
2018-11-29 20:11:06 +0000

    feat: Add code for coroutines

---
+++ appendix_a_coroutines/Drum Machine/src/main/kotlin/Beats.kt
@@ -0,0 +1,38 @@
+import java.io.File
+import javax.sound.sampled.AudioSystem
+
+import kotlinx.coroutines.*
+
+suspend fun playBeats(beats: String, file: String) {
+    val parts = beats.split("x")
+    var count = 0
+    for (part in parts) {
+        count += part.length + 1
+        if (part == "") {
+            playSound(file)
+        } else {
+            delay(100 * (part.length + 1L))
+            if (count < beats.length) {
+                playSound(file)
+            }
+        }
+    }
+}
+
+fun playSound(file: String) {
+    val clip = AudioSystem.getClip()
+    val audioInputStream = AudioSystem.getAudioInputStream(
+        File(
+            file
+        )
+    )
+    clip.open(audioInputStream)
+    clip.start()
+}
+
+suspend fun main() {
+    GlobalScope.launch {
+        playBeats("x-x-x-x-x-x-", "toms.aiff")
+    }
+    playBeats("x-----x-----", "crash_cymbal.aiff")
+}
