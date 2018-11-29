bc0df7836a627b032205086a80f0d1b299f963f8
David Griffiths <dogriffiths@gmail.com>
2018-11-29 19:38:22 +0000

    feat: Add code for chapter 10

---
+++ chapter10/Generics/src/Pets.kt
@@ -0,0 +1,88 @@
+abstract class Pet(var name: String)
+
+class Cat(name: String) : Pet(name)
+
+class Dog(name: String) : Pet(name)
+
+class Fish(name: String) : Pet(name)
+
+class Vet<T : Pet> {
+    fun treat(t: T) {
+        println("Treat Pet ${t.name}")
+    }
+}
+
+class Contest<T : Pet>(var vet: Vet<in T>) {
+    val scores: MutableMap<T, Int> = mutableMapOf()
+
+    fun addScore(t: T, score: Int = 0) {
+        if (score >= 0) scores.put(t, score)
+    }
+
+    fun getWinners(): MutableSet<T> {
+        val winners: MutableSet<T> = mutableSetOf()
+        val highScore = scores.values.max()
+        for ((t, score) in scores) {
+            if (score == highScore) winners.add(t)
+        }
+        return winners
+    }
+}
+
+interface Retailer<out T> {
+    fun sell(): T
+}
+
+class CatRetailer : Retailer<Cat> {
+    override fun sell(): Cat {
+        println("Sell Cat")
+        return Cat("")
+    }
+}
+
+class DogRetailer : Retailer<Dog> {
+    override fun sell(): Dog {
+        println("Sell Dog")
+        return Dog("")
+    }
+}
+
+class FishRetailer : Retailer<Fish> {
+    override fun sell(): Fish {
+        println("Sell Fish")
+        return Fish("")
+    }
+}
+
+fun main(args: Array<String>) {
+    val catFuzz = Cat("Fuzz Lightyear")
+    val catKatsu = Cat("Katsu")
+    val fishFinny = Fish("Finny McGraw")
+
+    val catVet = Vet<Cat>()
+    val fishVet = Vet<Fish>()
+    val petVet = Vet<Pet>()
+
+    catVet.treat(catFuzz)
+    petVet.treat(catKatsu)
+    petVet.treat(fishFinny)
+
+    val catContest = Contest<Cat>(catVet)
+    catContest.addScore(catFuzz, 50)
+    catContest.addScore(catKatsu, 45)
+    val topCat = catContest.getWinners().first()
+    println("Cat contest winner is ${topCat.name}")
+
+    val petContest = Contest<Pet>(petVet)
+    petContest.addScore(catFuzz, 50)
+    petContest.addScore(fishFinny, 56)
+    val topPet = petContest.getWinners().first()
+    println("Pet contest winner is ${topPet.name}")
+
+    val fishContest = Contest<Fish>(petVet)
+
+    val dogRetailer: Retailer<Dog> = DogRetailer()
+    val catRetailer: Retailer<Cat> = CatRetailer()
+    val petRetailer: Retailer<Pet> = CatRetailer()
+    petRetailer.sell()
+}
