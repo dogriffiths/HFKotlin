a4bf266dd06958a950759d904253cb1601fd872f
David Griffiths <dogriffiths@gmail.com>
2018-12-02 15:12:55 +0000

    Updates

--- precache-manifest.76ed6d7f3b1bbd43797529d403b8d607.js
+++ precache-manifest.76ed6d7f3b1bbd43797529d403b8d607.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "https://dogriffiths.github.io/HFKotlin/static/js/runtime~main.32a1f91a.js"
   },
   {
-    "revision": "23df887112eae8e0a0f8",
-    "url": "https://dogriffiths.github.io/HFKotlin/static/js/main.23df8871.chunk.js"
+    "revision": "d6a8c18b86a088f3ab91",
+    "url": "https://dogriffiths.github.io/HFKotlin/static/js/main.d6a8c18b.chunk.js"
   },
   {
     "revision": "cb91c2305afecb2bad3f",
     "url": "https://dogriffiths.github.io/HFKotlin/static/js/1.cb91c230.chunk.js"
   },
   {
-    "revision": "23df887112eae8e0a0f8",
+    "revision": "d6a8c18b86a088f3ab91",
     "url": "https://dogriffiths.github.io/HFKotlin/static/css/main.9bc9f712.chunk.css"
   },
   {
-    "revision": "d23eeaad9703e75f1bcf565d23787b83",
+    "revision": "3df68823d44f1c1efb00c8245b56af58",
     "url": "https://dogriffiths.github.io/HFKotlin/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://dogriffiths.github.io/HFKotlin/precache-manifest.68c563434498a01fc35c4dde2d7cbca5.js"
+  "https://dogriffiths.github.io/HFKotlin/precache-manifest.76ed6d7f3b1bbd43797529d403b8d607.js"
 );
 
 workbox.clientsClaim();
