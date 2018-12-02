a283b5f0be28475b4034708c5d4e8eb63db3724f
David Griffiths <dogriffiths@gmail.com>
2018-12-02 14:27:22 +0000

    Updates

---
+++ precache-manifest.ced0772ce289933a3661a709fdc57d3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32a1f91a256531b8eb5b",
+    "url": "https://dogriffiths.github.io/HFKotlin/static/js/runtime~main.32a1f91a.js"
+  },
+  {
+    "revision": "aad7ec81ce32d6032af9",
+    "url": "https://dogriffiths.github.io/HFKotlin/static/js/main.aad7ec81.chunk.js"
+  },
+  {
+    "revision": "665c8b3834d39c7c412f",
+    "url": "https://dogriffiths.github.io/HFKotlin/static/js/1.665c8b38.chunk.js"
+  },
+  {
+    "revision": "aad7ec81ce32d6032af9",
+    "url": "https://dogriffiths.github.io/HFKotlin/static/css/main.3914bd76.chunk.css"
+  },
+  {
+    "revision": "b2640f25e3dfafececdb1b1d59b90997",
+    "url": "https://dogriffiths.github.io/HFKotlin/index.html"
+  }
+];
